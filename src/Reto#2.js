d43338d267cd1650da8d09464a4b19af90b0c2fd
Juan Gomez <jf.gomezramiez@gmail.com>
2022-01-15 23:14:58 -0500

    Segundo reto terminado

---
+++ src/Reto#2.js
@@ -0,0 +1,45 @@
+// Transforma el texto a un objeto que contenga el nombre de cada regalo y las veces que aparece. Por ejemplo, si tenemos el texto:
+
+// const carta = 'bici coche balón _playstation bici coche peluche'
+
+// Al ejecutar el método debería devolver lo siguiente:
+
+// const regalos = listGifts(carta)
+
+// console.log(regalos)
+/*
+{
+  bici: 2,
+  coche: 2,
+  balón: 1,
+  peluche: 1
+}
+*/
+
+const carta = 'bici  coche   balón _playstation bici coche peluche'
+
+const listGifts = (str) => {
+  let count = {};
+  str
+    .split(' ')
+    .filter((item) => !item.startsWith('_'))
+    .filter((item) => !item == '')
+    .forEach((x) =>  count[x] = (count[x] || 0) + 1);
+  return count
+}
+
+console.log(listGifts(carta));
+
+
+var array = [ 'bici', 'coche', 'balón', 'bici', 'coche', 'peluche' ];
+
+
+var counts = {};
+
+console.log(counts);
+
+array.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+
+console.log(array);
+
+console.log(counts);
